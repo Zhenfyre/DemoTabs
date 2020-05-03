58c2e1ab0e13c03c1dfad242a9c047f3400c0cf1
Abora <https://github.com/abora97>
2020-05-03 02:21:10 +0200

    create home and add dataBinding

--- app/src/main/java/com/example/testtabslay/HomeActivity.java
+++ app/src/main/java/com/example/testtabslay/HomeActivity.java
@@ -1,14 +1,64 @@
 package com.example.testtabslay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.os.Bundle;
 
+import com.example.testtabslay.databinding.ActivityHomeBinding;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class HomeActivity extends AppCompatActivity {
+
+   private ActivityHomeBinding binding;
+
+    // tab titles
+    private String[] titles = new String[]{"Movies", "Events", "Tickets"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        init();
+    }
+    private void init() {
+        // removing toolbar elevation
+        getSupportActionBar().setElevation(0);
+
+        binding.viewPager.setAdapter(new ViewPagerFragmentAdapter(this));
+
+        // attaching tab mediator
+        new TabLayoutMediator(binding.tabLayout, binding.viewPager,
+                (tab, position) -> tab.setText(titles[position])).attach();
+    }
+
+    private class ViewPagerFragmentAdapter extends FragmentStateAdapter {
+
+        public ViewPagerFragmentAdapter(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    return new MoviesFragment();
+                case 1:
+                    return new EventsFragment();
+                case 2:
+                    return new TicketsFragment();
+            }
+            return new MoviesFragment();
+        }
+
+        @Override
+        public int getItemCount() {
+            return titles.length;
+        }
     }
 }
